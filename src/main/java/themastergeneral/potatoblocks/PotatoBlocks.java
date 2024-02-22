2994283914f925f2b0bd2ef4a115413ef30be200
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-02-21 21:56:51 -0500

    1.18 specific fixes

--- src/main/java/themastergeneral/potatoblocks/PotatoBlocks.java
+++ src/main/java/themastergeneral/potatoblocks/PotatoBlocks.java
@@ -15,9 +15,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("potatoblocks")
